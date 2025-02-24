8a2ce06f7fe1118cbf8cd7dfe91948ca3bee3aa3
Trần Sỹ Minh Quân <minhquantsy@gmail.com>
2025-02-24 22:04:35 +0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,24 @@
+"""
+This file contains all the constants used in the project.
+"""
+
+RESULTS_DIR = "./results/"
+LOGGING_STEPS = 86
+SEED = 42
+
+# [I - CODE SUMMARIZATION]
+RESULTS_CS_DIR = RESULTS_DIR + "code-summarization/"
+CS_INPUT_MAX_LENGTH = 256
+CS_OUTPUT_MAX_LENGTH = 128
+CS_NUM_BEAMS = 5
+
+# [1. CODE BERT HYPERPARAMETERS]
+MODEL_BERT_NER = "dbmdz/bert-large-cased-finetuned-conll03-english"
+TOKENIZER_BERT = "dbmdz/bert-large-cased-finetuned-conll03-english"
+RESULTS_BERT_DIR = RESULTS_CS_DIR + "code-bert/"
+
+LR_BERT = 8e-5
+TRAIN_BATCH_SIZE_BERT = 16
+EVAL_BATCH_SIZE_BERT = 16
+NUM_TRAIN_EPOCHS_BERT = 50
+WEIGHT_DECAY_BERT = 0.01
