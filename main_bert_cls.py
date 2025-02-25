968ff64e9dfb4c908763f53c6519791dceceeccc
veil <auphong2707@gmail.com>
2025-02-25 15:53:57 +0700

    Update main_bert_cls.py

--- main_bert_cls.py
+++ main_bert_cls.py
@@ -19,7 +19,9 @@ metric = evaluate.load("seqeval")
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
-    print(predictions.shape)
+    print(type(predictions))
+    for x in predictions:
+        print(type(x))
     
     true_labels = [[ID2LABEL[l] for l in label if l != -100] for label in labels]
     pred_labels = [[ID2LABEL[p] for p, l in zip(pred, label) if l != -100] for pred, label in zip(predictions, labels)]
