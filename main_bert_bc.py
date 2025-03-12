7848f13ea36fb81937f9719ebdc9ff23bde95e96
Dmint474 <vuducminh474@gmail.com>
2025-03-12 11:50:04 +0700

    Update main_bert_bc.py

--- main_bert_bc.py
+++ main_bert_bc.py
@@ -13,7 +13,7 @@ wandb.login(key=os.getenv("WANDB_API_KEY"))
 huggingface_hub.login(token=os.getenv("HUGGINGFACE_TOKEN"))
 
 # Prepare the dataset and tokenizer
-train_dataset, val_dataset, test_dataset, tokenizer = prepare_dataset(TOKENIZER_BERT)
+train_dataset, val_dataset, test_dataset, tokenizer = prepare_dataset(TOKENIZER_BERT_BC)
 
 # Define compute_metrics function
 metric = evaluate.load("seqeval")
@@ -35,26 +35,26 @@ def compute_metrics(eval_pred):
     return metric.compute(predictions=decoded_preds, references=decoded_labels)
 
 # Setup results directory
-os.makedirs(EXPERIMENT_RESULTS_DIR, exist_ok=True)
+os.makedirs(EXPERIMENT_RESULTS_BBC_DIR, exist_ok=True)
 
 # Load model
-model = BERT_BiLSTM_CRF.from_pretrained(MODEL_BERT_NER, num_labels=len(ID2LABEL), ignore_mismatched_sizes=True)
+model = BERT_BiLSTM_CRF.from_pretrained(MODEL_BERT_BC, num_labels=len(ID2LABEL), ignore_mismatched_sizes=True)
 
 # Setup Training Arguments
 training_args = TrainingArguments(
-    run_name=EXPERIMENT_NAME,
+    run_name=EXPERIMENT_NAME_BERT_BC,
     report_to="wandb",
     evaluation_strategy='steps',
     save_strategy='steps',
-    eval_steps=EVAL_STEPS_BERT,
-    save_steps=SAVE_STEPS_BERT,
-    per_device_train_batch_size=TRAIN_BATCH_SIZE_BERT,
-    per_device_eval_batch_size=EVAL_BATCH_SIZE_BERT,
-    num_train_epochs=NUM_TRAIN_EPOCHS_BERT,
-    weight_decay=WEIGHT_DECAY_BERT,
-    learning_rate=LR_BERT, 
-    output_dir=EXPERIMENT_RESULTS_DIR,
-    logging_dir=EXPERIMENT_RESULTS_DIR + "/logs",
+    eval_steps=EVAL_STEPS_BERT_BC,
+    save_steps=SAVE_STEPS_BERT_BC,
+    per_device_train_batch_size=TRAIN_BATCH_SIZE_BERT_BC,
+    per_device_eval_batch_size=EVAL_BATCH_SIZE_BERT_BC,
+    num_train_epochs=NUM_TRAIN_EPOCHS_BERT_BC,
+    weight_decay=WEIGHT_DECAY_BERT_BC,
+    learning_rate=LR_BERT_BC, 
+    output_dir=EXPERIMENT_RESULTS_BBC_DIR,
+    logging_dir=EXPERIMENT_RESULTS_BBC_DIR + "/logs",
     logging_steps=LOGGING_STEPS,
     load_best_model_at_end=True,
     metric_for_best_model="eval_overall_f1",
@@ -86,20 +86,20 @@ trainer.train()
 test_results = trainer.evaluate(test_dataset, metric_key_prefix="test")
 
 # Save model, tokenizer, and training results
-model.save_pretrained(EXPERIMENT_RESULTS_DIR)
-tokenizer.save_pretrained(EXPERIMENT_RESULTS_DIR)
+model.save_pretrained(EXPERIMENT_RESULTS_BBC_DIR)
+tokenizer.save_pretrained(EXPERIMENT_RESULTS_BBC_DIR)
 
 # Save the training arguments and test results
-with open(EXPERIMENT_RESULTS_DIR + "/training_args.txt", "w") as f:
+with open(EXPERIMENT_RESULTS_BBC_DIR + "/training_args.txt", "w") as f:
     f.write(str(training_args))
 
-with open(EXPERIMENT_RESULTS_DIR + "/test_results.txt", "w") as f:
+with open(EXPERIMENT_RESULTS_BBC_DIR + "/test_results.txt", "w") as f:
     f.write(str(test_results))
 
 # Upload to Hugging Face
 api = huggingface_hub.HfApi()
 api.upload_large_folder(
-    folder_path=RESULTS_BERT_DIR,
+    folder_path=RESULTS_BERT_BC_DIR,
     repo_id="auphong2707/nlp-ner",
     repo_type="model",
     private=False
