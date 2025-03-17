df43d47cefafbc4cab81d04b186c31b54420ea7e
Sơn Tùng Kiều <kieusontung8@gmail.com>
2025-03-17 12:06:02 +0700

    fix bert_config

--- model_bert_crf.py
+++ model_bert_crf.py
@@ -1,11 +1,12 @@
 import torch 
 import torch.nn as nn
 from TorchCRF import CRF
-from transformers import BertModel, BertPreTrainedModel
+from transformers import BertModel, BertPreTrainedModel, BertConfig
 
 class Bert_CRF(BertPreTrainedModel):
     def __init__(self, bert_model_name, num_labels, hidden_dim=256, dropout=0.3):
-        super().__init__()
+        bert_config = BertConfig.from_pretrained(bert_model_name)
+        super().__init__(bert_config)
 
         #Bert model
         self.bert = BertModel.from_pretrained(bert_model_name)
