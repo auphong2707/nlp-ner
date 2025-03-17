2cd992349b3ad500914240d4da3368bb60fbd556
Sơn Tùng Kiều <kieusontung8@gmail.com>
2025-03-17 13:45:07 +0700

    fix

--- main_bert_crf.py
+++ main_bert_crf.py
@@ -18,19 +18,56 @@ set_seed(SEED)
 wandb.login(key=os.getenv("WANDB_API_KEY"))
 huggingface_hub.login(token=os.getenv("HUGGINGFACE_TOKEN"))
 
-def collate_fn(batch):
-    input_ids = torch.tensor([item['input_ids'] for item in batch],dtype=torch.long)
-    attention_mask = torch.tensor([item['attention_mask'] for item in batch], dtype=torch.long)
-    labels = torch.tensor([item['labels'] for item in batch], dtype=torch.long)
-    return input_ids, attention_mask, labels
-
 # Load dataset and tokenizer
 train_dataset, val_dataset, test_dataset, tokenizer = prepare_dataset(TOKENIZER_BERT_CRF)
-train_loader = DataLoader(train_dataset,batch_size=TRAIN_BATCH_SIZE_BERT_CRF, shuffle=True, num_workers=4, collate_fn=collate_fn)
-val_loader = DataLoader(val_dataset,batch_size=EVAL_BATCH_SIZE_BERT_CRF,shuffle=False,num_workers=4,collate_fn=collate_fn)
-test_loader = DataLoader(test_dataset,batch_size=EVAL_BATCH_SIZE_BERT_CRF,shuffle=False,num_workers=4,collate_fn=collate_fn)
+
+# Define compute_metrics function
+metric = evaluate.load("seqeval")
+
+def compute_metrics(eval_pred):
+    preds, labels = eval_pred
+    print(f"preds.shape, labels.shape{preds.shape, labels.shape}")
+    # Convert preds and labels to lists if numpy arrays
+    if isinstance(preds, np.ndarray):
+        preds = preds.tolist()
+    if isinstance(labels,np.ndarray):
+        labels = labels.tolist()
+
+    decoded_labels = []
+    decoded_preds = []
+
+    for label_seq, pred_seq in zip(labels, preds):
+        # if not isinstance(label_seq, list) or not isinstance(pred_seq, list):
+        #     continue
+        
+        current_labels = []
+        current_preds = []
+
+        for label, pred in zip(label_seq, pred_seq):
+            # if isinstance(label, (float, np.float32)) or isinstance(pred, (float,np.float32)):
+            #     continue
+            if label!=31:
+                current_labels.append(ID2LABEL.get(label,"0"))
+                current_preds.append(ID2LABEL.get(pred,"0"))
+        
+        if current_labels and current_preds:
+            decoded_labels.append(current_labels)
+            decoded_preds.append(current_preds)
+
+    print("Final decoded_preds sample:", decoded_preds[:5])
+    print("Final decoded_labels sample:", decoded_labels[:5])
+
+    if not decoded_preds or not decoded_labels:
+        print("Warning: No valid predictions or labels found!")
+        return {"eval_precision": 0.0, "eval_recall": 0.0, "eval_f1": 0.0}
+    
+    return metric.compute(predictions=decoded_preds,references=decoded_labels)
+
 
 "[SETTING UP MODEL AND TRAINING ARGUMENTS]"
+# Create results directory if not exists
+os.makedirs(EXPERIMENT_RESULTS_BERT_CRF_DIR, exist_ok=True)
+
 # Load checkpoint if available
 def get_last_checkpoint(ouput_dir):
     if not os.path.exists(ouput_dir):
@@ -41,14 +78,11 @@ def get_last_checkpoint(ouput_dir):
         return os.path.join(ouput_dir,last_checkpoint)
     return None
 
-# Create results directory if not exists
-os.makedirs(EXPERIMENT_RESULTS_BERT_CRF_DIR, exist_ok=True)
-
 checkpoint = get_last_checkpoint(EXPERIMENT_RESULTS_BERT_CRF_DIR)
 if checkpoint:
     model = Bert_CRF.from_pretrained(checkpoint)
 else:
-    model = Bert_CRF(MODEL_BERT_CRF,num_labels=len(ID2LABEL))
+    model = Bert_CRF(MODEL_BERT_CRF,num_labels=len(ID2LABEL),ignore_mismatched_sizes=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
@@ -77,47 +111,6 @@ training_args = TrainingArguments(
     max_grad_norm=1.0
 )
 
-# Define compute_metrics function
-metric = evaluate.load("seqeval")
-
-def compute_metrics(eval_pred):
-    preds, labels = eval_pred
-    # Convert preds and labels to lists if numpy arrays
-    if isinstance(preds, np.ndarray):
-        preds = preds.tolist()
-    if isinstance(labels,np.ndarray):
-        labels = labels.tolist()
-
-    decoded_labels = []
-    decoded_preds = []
-
-    for label_seq, pred_seq in zip(labels, preds):
-        if not isinstance(label_seq, list) or not isinstance(pred_seq, list):
-            continue
-        
-        current_labels = []
-        current_preds = []
-
-        for label, pred in zip(label_seq, pred_seq):
-            if isinstance(label, (float, np.float32)) or isinstance(pred, (float,np.float32)):
-                continue
-            if label!=31:
-                current_labels.append(ID2LABEL.get(label,"0"))
-                current_preds.append(ID2LABEL.get(pred,"0"))
-        
-        if current_labels and current_preds:
-            decoded_labels.append(current_labels)
-            decoded_preds.append(current_preds)
-
-    print("Final decoded_preds sample:", decoded_preds[:5])
-    print("Final decoded_labels sample:", decoded_labels[:5])
-
-    if not decoded_preds or not decoded_labels:
-        print("Warning: No valid predictions or labels found!")
-        return {"eval_precision": 0.0, "eval_recall": 0.0, "eval_f1": 0.0}
-    
-    return metric.compute(predictions=decoded_preds,references=decoded_labels)
-
 # Create Trainer instance
 trainer = Trainer(
     model=model,
@@ -129,8 +122,10 @@ trainer = Trainer(
 )
 
 "[TRAINING]"
-# Training loop
-trainer.train()
+if checkpoint:
+    trainer.train(resume_from_checkpoint=checkpoint)
+else:
+    trainer.train()
 
 "[EVALUATING]"
 # Evaluation
